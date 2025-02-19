e4dc9bb64ecd0678ccaa876333ecf09a1cf3b554
Haidepptrai <hainguyenhoangcontact@gmail.com>
2025-02-19 21:02:47 +0700

    fix: relationship pending changed

---
+++ ETutoring.DataAccess/Migrations/20250219140214_Update_User.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETutoring.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_blogs_users_user_id",
+                table: "blogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_blogs_application_user_user_id",
+                table: "blogs",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_blogs_application_user_user_id",
+                table: "blogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_blogs_users_user_id",
+                table: "blogs",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
