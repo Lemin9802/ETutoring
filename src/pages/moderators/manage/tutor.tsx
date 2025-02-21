70200a28ded29bc4ac4074734d3a8ee316763556
HoangIT754 <133497646+HoangIT754@users.noreply.github.com>
2025-02-21 23:46:31 +0700

    update-ui-manage-tutors-for-moderator

--- src/pages/moderators/manage/tutor.tsx
+++ src/pages/moderators/manage/tutor.tsx
@@ -1,41 +1,42 @@
 import React from "react";
-import TutorList, { TutorType } from "../components/managelist/tutorList";
+import TutorList from "../components/managelist/tutorList";
+// import TutorList, { TutorType } from "../components/managelist/tutorList";
 
-const tutorsData: TutorType[] = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-  {
-    key: "4",
-    name: "New Guy",
-    age: 25,
-    address: "Some Address",
-    tags: ["new tag"],
-  }
-];
+// const tutorsData: TutorType[] = [
+//   {
+//     key: "1",
+//     name: "John Brown",
+//     age: 32,
+//     address: "New York No. 1 Lake Park",
+//     tags: ["nice", "developer"],
+//   },
+//   {
+//     key: "2",
+//     name: "Jim Green",
+//     age: 42,
+//     address: "London No. 1 Lake Park",
+//     tags: ["loser"],
+//   },
+//   {
+//     key: "3",
+//     name: "Joe Black",
+//     age: 32,
+//     address: "Sydney No. 1 Lake Park",
+//     tags: ["cool", "teacher"],
+//   },
+//   {
+//     key: "4",
+//     name: "New Guy",
+//     age: 25,
+//     address: "Some Address",
+//     tags: ["new tag"],
+//   }
+// ];
 
 const TutorPage: React.FC = () => {
   return (
     <div>
-      <TutorList data={tutorsData} />
+      <TutorList />
     </div>
   );
 };
