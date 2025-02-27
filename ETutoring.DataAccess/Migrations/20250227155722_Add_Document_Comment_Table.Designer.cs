7bcca11a63dce589066ad768651220d13837f02e
Haidepptrai <hainguyenhoangcontact@gmail.com>
2025-02-27 22:57:54 +0700

    feat: document comment and testing

--- ETutoring.DataAccess/Migrations/20250227155722_Add_Document_Comment_Table.Designer.cs
+++ ETutoring.DataAccess/Migrations/20250227155722_Add_Document_Comment_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ETutoring.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ETutoring.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250227155722_Add_Document_Comment_Table")]
+    partial class Add_Document_Comment_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
