fb23967d71b9639112354ea6e8e22725502a9fb4
Lemin9802 <94817700+Lemin9802@users.noreply.github.com>
2025-03-12 16:47:22 +0700

    Update Blog API

--- ETutoring.Business/Interfaces/Services/IBlogService.cs
+++ ETutoring.Business/Interfaces/Services/IBlogService.cs
@@ -1,13 +1,14 @@
 ﻿using ETutoring.Business.Dtos.Blogs;
 using ETutoring.Core.Entities;
 
-namespace ETutoring.Business.Interfaces.Services;
-
-public interface IBlogService
+namespace ETutoring.Business.Interfaces.Services
 {
-    public Task<Blog> CreateBlogAsync(CreateBlogRequest blog, CancellationToken cancellationToken);
-    public Task<List<Blog>> GetAllBlogsAsync(Guid userId, bool isAdmin, CancellationToken cancellationToken);
-    public Task<Blog?> GetBlogByIdAsync(Guid blogId, CancellationToken cancellationToken);
-    public Task<Blog?> UpdateBlogAsync(UpdateBlogRequest request, Guid userId, bool isAdmin, CancellationToken cancellationToken);
-    public Task<bool> DeleteBlogAsync(DeleteBlogRequest request, Guid userId, bool isAdmin, CancellationToken cancellationToken);
+    public interface IBlogService
+    {
+        Task<Blog> CreateBlogAsync(CreateBlogRequest request, CancellationToken cancellationToken);
+        Task<List<Blog>> GetAllBlogsAsync(Guid userId, bool isAdmin, CancellationToken cancellationToken);
+        Task<Blog?> GetBlogByIdAsync(Guid blogId, Guid userId, bool isAdmin, CancellationToken cancellationToken);
+        Task<Blog?> UpdateBlogAsync(Guid blogId, UpdateBlogRequest request, Guid userId, bool isAdmin, CancellationToken cancellationToken);
+        Task<bool> DeleteBlogAsync(Guid blogId, Guid userId, bool isAdmin, CancellationToken cancellationToken);
+    }
 }
